ad94a4add95b9dad788649c5f4a37a8700753c0d
Lena Blechmann <lenablechmann@gmail.com>
2020-12-02 20:10:01 +0100

    added user input check

--- controller.c
+++ controller.c
@@ -1,11 +1,17 @@
 // will have the following functions:
 // search_db(), which will take the date string and search the database for entries, will return a bool
 
-// write(), which will take the date string, use search_db, then if entry exists cat onto the existing
+// write(), 
+// prints: how was the day? and accepts user input
+// which will take the date string, use search_db, then if entry exists cat onto the existing
 // if entry doesn't exist, make a new entry for the date, returns bool for success
 
-// read(), which will take the date string, use search_db, and either print out entry to screen, or return error to main
+// read(), 
+// prints here is your entry from date: and then the entry itself
+// which will take the date string, use search_db, and either print out entry to screen, or return error to main
 // which will have to apologize (read returns bool)
 
 // delete(), which will take the date string, use search_db, and delete entry if finds it.
 // returns false if nothing can be found
+
+// all functions should return boolean (true if successful, false if some error)
--- main.c
+++ main.c
@@ -1,27 +1,31 @@
 /* Responsible for parsing user input, calling functions and exiting the program after */
 
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
-// write a main function that takes user input. "diary write today" or similar
+int main(int argc, char *argv[])
+{
+    // User input needs to have 3 parts (program name, what do, and date).
+    if (argc != 3)
+    {
+        printf("Usage: diary command date.\n Like: diary write today \n");
+        exit(EXIT_FAILURE);
+    }
+    // Sends the date string to date.c, which decides if it's an alright date.
 
-// checks user input for errors and edge cases
-// needs to have 3 parts.
-// dates are only acceptable if they have the form "today", "yesterday", YYYY-MM-DD or DD.MM.YYYY or MM/DD/YYYY
-// checks general date format, prints error if wrong, if date format is ok, sends it to date.c
+    // Checks general command input. make use of tolower(), also w, r, d, rm, and -r -w -d -rm are ok
 
-// checks general command input. make use of tolower(), also w, r, d, rm, and -r -w -d -rm are ok
+    // w, write, -w, should print "How was your day?",
+    // call the write function from controller.c,
+    // which will save the user input to the database (or cat it if the entry already exists)
+    // until the user inputs the ending
 
-// w, write, -w, should print "How was your day?", 
-// call the write function from controller.c, 
-// which will save the user input to the database (or cat it if the entry already exists)
-// until the user inputs the ending
+    // r, -r, read should call the read function from controller.c, which will print the required entry
+    // if that entry exists (make a separate search db function in controller)
 
-// r, -r, read should call the read function from controller.c, which will print the required entry
-// if that entry exists (make a separate search db function in controller)
+    // if user input is delete, check if there is an entry with the date,
+    // if so, delete the entry
 
-// if user input is delete, check if there is an entry with the date,
-// if so, delete the entry
-
-// once finished exit the program
+    // once finished and got "true" from the function it called, exit the program
+}
