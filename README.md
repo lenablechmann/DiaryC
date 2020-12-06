1377ae3bc54c90d47ec9b378965dabb10248317e
Lena Blechmann <lenablechmann@gmail.com>
2020-12-06 13:04:43 +0100

    Added a date plausibility check (leap year dependent)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 A simple diary written in C which will take user input and save it into a SQLite table.
+
+TODO in the README:
+- add a gif of the app in action
+- a list of features
+- Installation and dependencies
+- how to use
+- potential upcoming features
--- date.c
+++ date.c
@@ -11,14 +11,14 @@
 
 char *parse_date_string(char *user_date)
 {
-    // adding a final result date string
+    // the final result will be stored in this string
     char date_final[DATE_LENGTH] = {0};
     char *final_string = date_final;
 
     // ints for error checking user input dates
     int day = 0, month = 0, year = 0;
 
-    // turning array into string
+    // turning array into string and checking base length
     int length = strlen(user_date);
     char *input_string_ptr = user_date;
 
@@ -40,6 +40,7 @@ char *parse_date_string(char *user_date)
                 date[i] = tolower(*input_string_ptr);
             }
         }
+
         // once we set everything to lower, the pointer can point to the new char array and make it a string
         input_string_ptr = date;
 
@@ -56,7 +57,7 @@ char *parse_date_string(char *user_date)
 
         // Assuming user input is a numeric date then. Parse it into ints.
         sscanf(user_date, " %d.%d.%d ", &day, &month, &year);
-    
+
         int plausibility = date_plausibility(day, month, year);
 
         if (plausibility == 1)
@@ -76,13 +77,45 @@ int date_plausibility(const int day, const int month, const int year)
 {
     // checks date plausibility (days in a month, leap year), returns 0 if date unplausible. 1 if plausible.
 
+    // first preliminary check; covering generally impossible things like negative days
     if (day <= 0 || day > 31 || month <= 0 || month > 12 || year <= 0 || year > 9999)
     {
-        return  0;
+        return 0;
     }
     else
     {
+        // Arrays for days per month in leap years and non leap years, 0 indexed so January is 0, December is 11:
+        int days_per_month_non[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days_per_month_leap[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        // check if year is a leap year
+        if (year % 4 == 0 && year % 100 != 0)
+        {
+            // definitely a leap year, checking if days are plausible via array. month - 1 cause arrays are 0 indexed
+            if (day > days_per_month_leap[month - 1])
+            {
+                return 0;
+            }
+        }
+
+        else if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+        {
+            // a leap year, checking if days are plausible via array. month - 1 cause arrays are 0 indexed
+            if (day > days_per_month_leap[month - 1])
+            {
+                return 0;
+            }
+        }
+
+        else
+        {
+            // not a leap year, checking if days are plausible via array. month -1 cause arrays are 0 indexed
+            if (day > days_per_month_non[month - 1])
+            {
+                return 0;
+            }
+        }
+
         return 1;
     }
-    
 }
