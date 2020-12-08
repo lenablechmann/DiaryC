6fe8e9f8d93feabf25c978a1428a3f94dc4147c9
Lena Blechmann <lenablechmann@gmail.com>
2020-12-08 20:45:38 +0100

    Added first sql insert query to the program

--- queries.c
+++ queries.c
@@ -12,8 +12,8 @@ bool search_db(char *date_string, size_t date_len)
 bool write_entry(char *date_string, size_t date_len)
 {
     printf("The size of this string is %ld and the string is %s\n", date_len, date_string);
-    //TODO remalloc date_string
 
+    //TODO realloc date_string if not enough has been allocated
     // initializing a string for the user input
     char *entry = (char*)(malloc((ENTRY_LEN + 1) * sizeof(char)));
     if (entry == NULL)
@@ -21,7 +21,6 @@ bool write_entry(char *date_string, size_t date_len)
         printf("Error allocating memory for the entry.\n");
         return false;
     }
-    printf("The date is %s\n", date_string);
 
     // Initializing the string to NUL
     for (int i = 0; i < ENTRY_LEN + 1; i++)
@@ -29,8 +28,10 @@ bool write_entry(char *date_string, size_t date_len)
         entry[i] ='\0';
     }
 
-    printf("Your entry:\n");
-    char input_tmp = 0;
+    printf("Write away (CTRL + D to save the entry and exit):\n");
+
+    // getc needs to be stored in an int, cause EOF is a negative int 
+    int input_tmp = 0;
     int count = 0;
 
     for (; count <= ENTRY_LEN; count++)
@@ -42,17 +43,59 @@ bool write_entry(char *date_string, size_t date_len)
         }
         entry[count] = input_tmp;
     }
-    printf("\nYour entry for the day %s is:\n", date_string);
+
+    /* printf("\nYour entry for the day %s is:\n", date_string);
     
     for (char *trace = entry; *trace != '\0'; trace++)
     {
         printf("%c", *trace);
     }
+    */
 
-    // prints: how was the day? and accepts user input 
     // which will take the date string, use search_db, then if entry exists cat onto the existing
     // if entry doesn't exist, make a new entry for the date, returns bool for success
+
+    // accessing the db testing
+    // SCHEMA: CREATE TABLE diary (id INTEGER PRIMARY KEY AUTOINCREMENT DEFAULT 0, date INTEGER NOT NULL,entry TEXT NOT NULL);
+    sqlite3 *db;
+    int result = sqlite3_open("diary.db", &db);
+
+    // Checking it opened successfully (is not 0)
+    if (result)
+    {
+        printf("Can't open database.\n");
+        sqlite3_close(db);
+        return 0;
+    }
+
+    // using prepared statements v2 (v3 is for special occasions according to doco)
+    // prepare_v2 helps you avoid sql injections https://www.sqlite.org/c3ref/prepare.html
+
+    sqlite3_stmt *stmt;
+    int err = sqlite3_prepare_v2(db, "INSERT INTO diary(date, entry) VALUES(?, ?)", -1, &stmt, NULL);
+    if (err != SQLITE_OK)
+        {
+            printf("Sql statement failed. %s \n", sqlite3_errmsg(db));
+            sqlite3_finalize(stmt);
+            sqlite3_close(db);
+            return 0;
+        }
+    // binding values (date and entry):
+    sqlite3_bind_text(stmt, 1, "2023-12-12", -1, NULL);
+    sqlite3_bind_text(stmt, 2, "just testing away.", -1, NULL);
+
+    // Inserts values into table via step
+    sqlite3_step(stmt);
+    // clearing memory alloced for the stmt
+    sqlite3_finalize(stmt);
+
+    // same story with update sql.
+    // now off to delete rows  DELETE FROM table WHERE day
+    sqlite3_close(db);
+    printf("successfully used the db\n");
+
     free(entry);
+
     return true;
 }
 
